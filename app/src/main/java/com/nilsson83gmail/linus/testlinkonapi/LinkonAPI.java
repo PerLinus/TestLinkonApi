b376e49a8fae5766238e485547dc02abcd604316
PerLinus <linus.nilsson83@gmail.com>
2018-01-18 09:20:28 +0100

    Initial commit

---
+++ app/src/main/java/com/nilsson83gmail/linus/testlinkonapi/LinkonAPI.java
@@ -0,0 +1,13 @@
+package com.nilsson83gmail.linus.testlinkonapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinkonAPI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linkon_api);
+    }
+}
